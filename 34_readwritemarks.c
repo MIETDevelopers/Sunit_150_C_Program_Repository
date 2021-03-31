aba13c7dfe3bf3a37a544bca33d12d10a19b0266
Sunit Padhiar <76240443+2020a1r150@users.noreply.github.com>
2021-03-31 12:38:25 +0530

    update

--- 34_readwritemarks.c
+++ 34_readwritemarks.c
@@ -1,29 +1,35 @@
-//A C Program to read .csv files.
-#include <stdio.h> //Including standard input output header files.
-#include <stdlib.h>//Including standard library header files.
-#include <string.h>//Including string header files. 
-
-const char* getfield(char* line, int num)
-{
-    const char* tok;
-    for (tok = strtok(line, ";");
-            tok && *tok;
-            tok = strtok(NULL, ";\n"))
-    {
-        if (!--num)
-            return tok;
-    }
-    return NULL;
-}
-int main()
-{
-    FILE* stream = fopen("E:\\Softwares\\C_Programs\\Ishav_160_C_Programs_Repository\\034_ReadCSV.csv", "r"); //Path of the .csv file.
-
-    char line[1024];
-    while (fgets(line, 1024, stream))
-    {
-        char* tmp = strdup(line);
-        printf("Field 3 would be %s\n", getfield(tmp, 3));
-        free(tmp);
-    }
+//A C program to Program for reading and writing the student marks data to files.				© Ishav Verma 21/March/2021
+#include <stdio.h>
+#include <stdlib.h>
+struct s {
+	//Creating a user defined data type using structures.
+	char name[50];
+	int RollNo;
+	int Marks;
+};
+int main() {
+	struct s a[10],b[10];
+	FILE *fptr;
+	int i;
+	fptr=fopen("E:\\Softwares\\C_Programs\\Ishav_160_C_Programs_Repository\\031_ReadWriteMarks.txt","wb"); //Opening the file from the location.
+	for (i=0;i<1;++i) {
+		fflush(stdin);
+		//Scanning the entered Values.
+		printf("Enter Name: ");
+		gets(a[i].name);
+		printf("Enter Roll Number: ");
+		scanf("%d",&a[i].RollNo);
+		printf("Enter Marks: ");
+		scanf("%d",&a[i].Marks);
+	}
+	//Writing program.
+	fwrite(a,sizeof(a),1,fptr);
+	fclose(fptr);
+	fptr=fopen("E:\\Softwares\\C_Programs\\Sunit_150_C_Programs_Repository\\031_ReadWriteMarks.txt","rb");
+	fread(b,sizeof(b),1,fptr);
+	printf("\n Entered details are:\n");
+	for (i=0;i<1;++i) {
+		printf("	Name: %s\n	Roll Number: %d\n	Marks: %d",b[i].name,b[i].RollNo,b[i].Marks); //Printing the entered text.
+	}
+	fclose(fptr); //Closing the program.
 }
--- 36_10elementsarray.c
+++ 36_10elementsarray.c
@@ -10,5 +10,6 @@ int main()
     }
     for (int i = 0; i < 10; i++)
     {
-        printf("%d",*(arr+i)); // array as a pointer
+        printf("%d", arr[i]);
     }
+}
